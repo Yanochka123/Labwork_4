c49dec48383960fea9c5e92ea09f600779ed64cd
Yanochka123 <78492211+Yanochka123@users.noreply.github.com>
2023-12-21 16:14:00 +0300

    Update testing_age_task.py

--- testing_age_task.py
+++ testing_age_task.py
@@ -3,7 +3,7 @@ import unittest
 from age_task import binary_search
 
 
-class SudokuTestCase(unittest.TestCase):
+class ClassTestCase(unittest.TestCase):
     def test_binary_search(self):
         values = [1, 2, 3, 4]
         age = 2
@@ -22,28 +22,6 @@ class SudokuTestCase(unittest.TestCase):
         expected_number = 7
         self.assertEqual(binary_search(
             values, 0, len(values)-1, age), expected_number)
-
-
-'''
-    def test_get_row(self):
-        puzzle = [["1", "2", "."], ["4", "5", "6"], ["7", "8", "9"]]
-        pos = (0, 0)
-        expected_row = ["1", "2", "."]
-        actual_row = sudoku.get_row(puzzle, pos)
-        self.assertEqual(expected_row, actual_row)
-
-        puzzle = [["1", "2", "3"], ["4", ".", "6"], ["7", "8", "9"]]
-        pos = (1, 0)
-        expected_row = ["4", ".", "6"]
-        actual_row = sudoku.get_row(puzzle, pos)
-        self.assertEqual(expected_row, actual_row)
-
-        puzzle = [["1", "2", "3"], ["4", "5", "6"], [".", "8", "9"]]
-        pos = (2, 0)
-        expected_row = [".", "8", "9"]
-        actual_row = sudoku.get_row(puzzle, pos)
-        self.assertEqual(expected_row, actual_row)
-'''
 
 
 if __name__ == '__main__':
